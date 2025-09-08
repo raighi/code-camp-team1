47c726783a980efdeb203e66ef4c94ec98d34949
Aloïs <alois.vincent@ik.me>
2025-09-08 22:57:54 +0200

    add(working basis): - updated commands_alois.py:         - fixed get command         - added every basic command - added main.py to test commands - made every file Flake8-compliant - made every file use commands_alois.py

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-import commands
+import commands_alois as commands
 from options import create_parser
 # Create command line parser
 options = create_parser().parse_args()
@@ -8,12 +8,11 @@ try:
         tasks = f.readlines()
     # Run the command
     if options.command == 'add':
-        commands.add(' '.join(options.details), options.file, tasks)
+        commands.add(details=' '.join(options.details), file=options.file)
     elif options.command == 'modify':
-        commands.modify(options.id, ' '.join(options.details), options.file,
-    tasks)
+        commands.modify(id=options.id, new_details=' '.join(options.details), file=options.file)
     elif options.command == 'rm':
-        commands.rm(options.id, options.file, tasks)
+        commands.rm(id=options.id, file=options.file)
     elif options.command == 'show':
         commands.show(tasks)
 except FileNotFoundError:

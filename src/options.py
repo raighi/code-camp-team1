568d0b35ecd3de0fa1711e06d7818296d74a3708
Raphael <raphael.hierso@gmail.com>
2025-09-11 10:20:25 +0200

    Update options.py
    Modification of the argument requirement options for the modify command

--- src/options.py
+++ src/options.py
@@ -20,10 +20,9 @@ def create_parser():
  # Création du parseur pour la commandemodify
  parser_modify = subparsers.add_parser('modify',help='Modify a task given its id. The rest of the command line is used for the task details, the default being "no details"')
  parser_modify.add_argument('id', help="the task id")
- parser_modify.add_argument('details', nargs='*', default="no details",
- help="the new details")
- parser_modify.add_argument('-u', '--user', nargs='*', help='New user associated with the task', default=None)
- parser_modify.add_argument('-t', '--estimatedtime', type=int, help='Set a new estimated time in seconds for performing the task', default=None)
+ parser_modify.add_argument('details', nargs='*', default="no details", help="the new details") 
+ parser_modify.add_argument('-u', '--user', nargs='*', required=False, type=str, help='New user associated with the task', default=None)
+ parser_modify.add_argument('-t', '--estimatedtime', required=False, type=int, help='Set a new estimated time for performing the task', default=None)
 
 
  # Création du parseur pour la commanderm
